a580d9b91cd9d41276506c7ffdd519a652216d89
Gurwinder Singh <gurwinder.61ds@gmail.com>
2016-04-25 11:14:02 +0530

    add this target

--- _protected/common/models/CompanyTechnologySearch.php
+++ _protected/common/models/CompanyTechnologySearch.php
@@ -7,6 +7,7 @@ use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use common\models\CompanyTechnology;
 
+
 /**
  * CompanyTechnologySearch represents the model behind the search form about `common\models\CompanyTechnology`.
  */
