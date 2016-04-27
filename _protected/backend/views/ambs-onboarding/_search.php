b3eb8c042a1dc43003583a9d07238cb976e1b266
Gurwinder Singh <gurwinder.61ds@gmail.com>
2016-04-27 15:26:05 +0530

    chapter sponser view

---
+++ _protected/backend/views/ambs-onboarding/_search.php
@@ -0,0 +1,93 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\AmbsOnboardingSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ambs-onboarding-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'first_name') ?>
+
+    <?= $form->field($model, 'last_name') ?>
+
+    <?= $form->field($model, 'email') ?>
+
+    <?= $form->field($model, 'country_code') ?>
+
+    <?php // echo $form->field($model, 'area_code') ?>
+
+    <?php // echo $form->field($model, 'phone_number') ?>
+
+    <?php // echo $form->field($model, 'chapter') ?>
+
+    <?php // echo $form->field($model, 'chapter_city') ?>
+
+    <?php // echo $form->field($model, 'chapter_state') ?>
+
+    <?php // echo $form->field($model, 'chapter_country') ?>
+
+    <?php // echo $form->field($model, 'twitter_handle') ?>
+
+    <?php // echo $form->field($model, 'chapter_email') ?>
+
+    <?php // echo $form->field($model, 'address') ?>
+
+    <?php // echo $form->field($model, 'street_address') ?>
+
+    <?php // echo $form->field($model, 'address_city') ?>
+
+    <?php // echo $form->field($model, 'address_state') ?>
+
+    <?php // echo $form->field($model, 'address_country') ?>
+
+    <?php // echo $form->field($model, 'address_zip') ?>
+
+    <?php // echo $form->field($model, 'file') ?>
+
+    <?php // echo $form->field($model, 'notes') ?>
+
+    <?php // echo $form->field($model, 'preferred_payment') ?>
+
+    <?php // echo $form->field($model, 'account_name') ?>
+
+    <?php // echo $form->field($model, 'bank_name') ?>
+
+    <?php // echo $form->field($model, 'bank_account') ?>
+
+    <?php // echo $form->field($model, 'aba_routing') ?>
+
+    <?php // echo $form->field($model, 'bank_address') ?>
+
+    <?php // echo $form->field($model, 'bank_street_address') ?>
+
+    <?php // echo $form->field($model, 'bank_city') ?>
+
+    <?php // echo $form->field($model, 'bank_state') ?>
+
+    <?php // echo $form->field($model, 'bank_country') ?>
+
+    <?php // echo $form->field($model, 'bank_zip') ?>
+
+    <?php // echo $form->field($model, 'paypal_email') ?>
+
+    <?php // echo $form->field($model, 'check_to') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ _protected/common/models/AmbsOnboarding.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+/**
+ * This is the model class for table "ambs_onboarding".
+ *
+ * @property integer $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property integer $country_code
+ * @property integer $area_code
+ * @property integer $phone_number
+ * @property integer $chapter
+ * @property integer $chapter_city
+ * @property integer $chapter_state
+ * @property integer $chapter_country
+ * @property string $twitter_handle
+ * @property string $chapter_email
+ * @property string $address
+ * @property string $street_address
+ * @property integer $address_city
+ * @property integer $address_state
+ * @property integer $address_country
+ * @property integer $address_zip
+ * @property string $file
+ * @property string $notes
+ * @property integer $preferred_payment
+ * @property string $account_name
+ * @property string $bank_name
+ * @property string $bank_account
+ * @property string $aba_routing
+ * @property string $bank_address
+ * @property string $bank_street_address
+ * @property integer $bank_city
+ * @property integer $bank_state
+ * @property integer $bank_country
+ * @property integer $bank_zip
+ * @property string $paypal_email
+ * @property string $check_to
+ *
+ * @property Chapters $chapter0
+ * @property Cities $addressCity
+ * @property States $addressState
+ * @property Countries $addressCountry
+ * @property Cities $bankCity
+ * @property States $bankState
+ * @property Countries $bankCountry
+ */
+class AmbsOnboarding extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'ambs_onboarding';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['first_name','bank_country','bank_state', 'last_name', 'email', 'country_code',  'phone_number', 'chapter', 'chapter_state', 'chapter_country', 'address', 'street_address', 'preferred_payment', 'address_state', 'address_country', 'address_zip', 'file', 'account_name', 'bank_name', 'bank_account', 'aba_routing', 'bank_address', 'bank_street_address', 'bank_zip', 'paypal_email', 'check_to'], 'required'],
+            [['country_code', 'area_code', 'phone_number', 'chapter', 'chapter_city', 'chapter_state', 'chapter_country', 'address_city', 'address_state', 'address_country', 'address_zip', 'preferred_payment', 'bank_city', 'bank_state', 'bank_country', 'bank_zip'], 'integer'],
+            [['notes'], 'string'],
+            [['first_name', 'last_name', 'email', 'twitter_handle', 'chapter_email', 'address', 'street_address', 'file', 'account_name', 'bank_name', 'bank_account', 'aba_routing', 'bank_address', 'bank_street_address','bank_city', 'bank_state', 'bank_country', 'paypal_email', 'check_to'], 'string', 'max' => 255],
+            [['chapter'], 'exist', 'skipOnError' => true, 'targetClass' => Chapters::className(), 'targetAttribute' => ['chapter' => 'id']],
+            [['address_city'], 'exist', 'skipOnError' => true, 'targetClass' => Cities::className(), 'targetAttribute' => ['address_city' => 'id']],
+            [['address_state'], 'exist', 'skipOnError' => true, 'targetClass' => States::className(), 'targetAttribute' => ['address_state' => 'id']],
+            [['address_country'], 'exist', 'skipOnError' => true, 'targetClass' => Countries::className(), 'targetAttribute' => ['address_country' => 'id']],
+            [['bank_city'], 'exist', 'skipOnError' => true, 'targetClass' => Cities::className(), 'targetAttribute' => ['bank_city' => 'id']],
+            [['bank_state'], 'exist', 'skipOnError' => true, 'targetClass' => States::className(), 'targetAttribute' => ['bank_state' => 'id']],
+            [['bank_country'], 'exist', 'skipOnError' => true, 'targetClass' => Countries::className(), 'targetAttribute' => ['bank_country' => 'id']],
+            [['paypal_email', 'chapter_city','check_to','account_name', 'bank_name', 'bank_account', 'aba_routing', 'bank_address', 'bank_street_address', 'bank_zip',],'safe']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'first_name' => Yii::t('app', 'First Name'),
+            'last_name' => Yii::t('app', 'Last Name'),
+            'email' => Yii::t('app', 'Email'),
+            'country_code' => Yii::t('app', 'Country Code'),
+            'area_code' => Yii::t('app', 'Area Code'),
+            'phone_number' => Yii::t('app', 'Phone Number'),
+            'chapter' => Yii::t('app', 'Chapter'),
+            'chapter_city' => Yii::t('app', 'Chapter City'),
+            'chapter_state' => Yii::t('app', 'State / Region '),
+            'chapter_country' => Yii::t('app', 'Chapter Country'),
+            'twitter_handle' => Yii::t('app', 'Aging2.0 Chapter twitter handle'),
+            'chapter_email' => Yii::t('app', 'Aging2.0 Chapter email address'),
+            'address' => Yii::t('app', 'Address'),
+            'street_address' => Yii::t('app', 'Street Address'),
+            'address_city' => Yii::t('app', 'Address City'),
+            'address_state' => Yii::t('app', 'State / Region '),
+            'address_country' => Yii::t('app', 'Address Country'),
+            'address_zip' => Yii::t('app', 'Address Zip'),
+            'file' => Yii::t('app', 'Alternatively, upload your completed W9 or W8'),
+            'notes' => Yii::t('app', 'Additional notes to Aging2.0 HQ'),
+            'preferred_payment' => Yii::t('app', 'Preferred Form of Payment '),
+            'account_name' => Yii::t('app', 'Account Name'),
+            'bank_name' => Yii::t('app', 'Bank Name'),
+            'bank_account' => Yii::t('app', 'Bank Account'),
+            'aba_routing' => Yii::t('app', 'Aba Routing'),
+            'bank_address' => Yii::t('app', 'Address'),
+            'bank_street_address' => Yii::t('app', 'Street Address'),
+            'bank_city' => Yii::t('app', 'City'),
+            'bank_state' => Yii::t('app', 'State / Region '),
+            'bank_country' => Yii::t('app', 'Country'),
+            'bank_zip' => Yii::t('app', 'Zip'),
+            'paypal_email' => Yii::t('app', 'Paypal Email'),
+            'check_to' => Yii::t('app', 'Check To'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getChapter0()
+    {
+        return $this->hasOne(Chapters::className(), ['id' => 'chapter']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAddressCity()
+    {
+        return $this->hasOne(Cities::className(), ['id' => 'address_city']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAddressState()
+    {
+        return $this->hasOne(States::className(), ['id' => 'address_state']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAddressCountry()
+    {
+        return $this->hasOne(Countries::className(), ['id' => 'address_country']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBankCity()
+    {
+        return $this->hasOne(Cities::className(), ['id' => 'bank_city']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBankState()
+    {
+        return $this->hasOne(States::className(), ['id' => 'bank_state']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBankCountry()
+    {
+        return $this->hasOne(Countries::className(), ['id' => 'bank_country']);
+    }
+    public function getCities(){
+        $cities = Cities::find()->where(['status' => 1])->orderBy('name')->all();
+        return ArrayHelper::map($cities,'id','name');
+    }
+
+    //get all states
+    public function getStates()
+    {
+        $states = States::find()->where(['status' => 1])->orderBy('name')->all();
+        return ArrayHelper::map($states,'id','name');
+    }
+
+    //get all countries
+    public function getCountries()
+    {
+        $countries = Countries::find()->where(['status' => 1])->orderBy('name')->all();
+        return ArrayHelper::map($countries,'id','name');
+    }
+    public function getChapters()
+    {
+        $spo_pay = Chapters::find()->where(['status' => 1])->orderBy('name')->all();
+        return ArrayHelper::map($spo_pay,'id','name');
+    }
+}
